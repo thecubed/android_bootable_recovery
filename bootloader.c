63d9750741162b6e06aab88b130b8e025f1dca14
Tyler Montgomery <tyler@mintoreo.(none)>
2012-01-30 01:07:22 -0800

    oops. needed that lib for malloc
    Change-Id: I806d735e9846dc113e2dff302936e14db77616f6

--- bootloader.c
+++ bootloader.c
@@ -24,6 +24,7 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 static int get_bootloader_message_mtd(struct bootloader_message *out, const Volume* v);
 static int set_bootloader_message_mtd(const struct bootloader_message *in, const Volume* v);
